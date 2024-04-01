02be3a57d30dcbc777a46c352dc51d9b6a8dcedb
Tmeeks1 <100161716+Tmeeks1@users.noreply.github.com>
2024-04-01 15:21:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -20,6 +21,11 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+#convert the snowpark dataframe to a pandas 
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
@@ -34,7 +40,7 @@ if ingredients_list:
 		st.subheader(fruit_chosen + ' Nutrition Information')
 		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
 		fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-    #st.write(ingredients_string)
+
 	my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 	
